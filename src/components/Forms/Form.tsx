5a9ffabda5b8f1fe20ec3eeb802eee36d7685fc3
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-09-26 00:05:42 +0600

    added more

---
+++ src/components/Forms/Form.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
+import { ReactElement, ReactNode } from "react";
+
+type FromConfig = {
+  defaultValues?: Record<string, any>;
+};
+
+type FromProps = {
+  children?: ReactElement | ReactNode;
+  submitHandler: SubmitHandler<any>;
+} & FromConfig;
+
+const Form = ({ children, submitHandler, defaultValues }: FromProps) => {
+  const fromConfig: FromConfig = {};
+  if (!!defaultValues) fromConfig["defaultValues"] = defaultValues;
+  const methods = useForm<FromProps>(fromConfig);
+  const { handleSubmit, reset } = methods;
+  console.log(methods);
+
+  const onSubmit = (data: any) => {
+    submitHandler(data);
+    reset();
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+};
+
+export default Form;
---
+++ src/components/Forms/FromInput.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Input } from "antd";
+import { type } from "os";
+import { Controller, useFormContext } from "react-hook-form";
+
+interface IInput {
+  type?: string;
+  name: string;
+  size?: "large" | "small";
+  value?: string | string[] | undefined;
+  id?: string;
+  placeholder?: string;
+  validation?: object;
+  label?: string;
+}
+
+const FromInput = ({
+  name,
+  size,
+  value,
+  type,
+  id,
+  placeholder,
+  label,
+  validation,
+}: IInput) => {
+  const { control } = useFormContext();
+
+  return (
+    <>
+      {label ? label : null}
+      <Controller
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <Input
+            type={type}
+            size={size}
+            placeholder={placeholder}
+            {...field}
+            value={value ? value : field.value}
+          />
+        )}
+      />
+    </>
+  );
+};
+
+export default FromInput;
