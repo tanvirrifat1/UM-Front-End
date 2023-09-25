74418df3e879225cf0f1a2e7ff725fd4e29d3e4f
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-09-25 21:29:19 +0600

    added more

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -3,50 +3,14 @@
 import React, { useState } from "react";
 const { Header, Content, Footer, Sider } = Layout;
 import { Breadcrumb, Layout, Menu, theme, MenuProps } from "antd";
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+
 import { sidebarItems } from "@/Constants/sidebarItems";
 import { USER_ROLE } from "@/Constants/role";
-type MenuItem = Required<MenuProps>["items"][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
-
-const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
-];
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
 
-  const role = USER_ROLE.ADMIN;
+  const role = USER_ROLE.STUDENT;
 
   return (
     <Sider
