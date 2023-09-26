8613c74bf1314e08916a06507b3e30f02ac7c1bc
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-09-26 16:05:05 +0600

    added login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,53 @@
+"use client";
+
+import { Row, Col, Button } from "antd";
 import React from "react";
+import img from "../../assets/loginImage/Login-amico.png";
+import Image from "next/image";
+import Form from "@/components/Forms/Form";
+import FromInput from "@/components/Forms/FromInput";
+import { SubmitHandler } from "react-hook-form";
+
+type FromValues = {
+  id?: string;
+  password?: string;
+};
 
 const LoginPage = () => {
+  const onSubmit: SubmitHandler<FromValues> = (data) => {
+    try {
+      console.log(data);
+    } catch (error) {}
+  };
+
   return (
     <div>
-      <h1>This is login page</h1>
+      <Row>
+        <Col sm={12} md={16} lg={16}>
+          <Image src={img} alt="" width={500} />
+        </Col>
+        <Col sm={12} md={8} lg={8}>
+          <h1>First login your account</h1>
+          <div>
+            <Form submitHandler={onSubmit}>
+              <div>
+                <FromInput name="id" type="text" size="large" label="User Id" />
+              </div>
+              <div>
+                <FromInput
+                  name="password"
+                  type="password"
+                  size="large"
+                  label="User Password"
+                />
+              </div>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+            </Form>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
