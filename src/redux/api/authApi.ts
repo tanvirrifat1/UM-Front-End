92f987f37b653a0337e07104e02e8d620d01161f
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-09-27 21:44:04 +0600

    added hooks

---
+++ src/redux/api/authApi.ts
@@ -0,0 +1,18 @@
+import { baseApi } from "./baseApi";
+
+const AUTH_RUL = "/auth";
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    userLogin: build.mutation({
+      query: (loginData) => ({
+        url: `${AUTH_RUL}/login`,
+        method: "POST",
+        data: loginData,
+      }),
+      invalidatesTags: ["user"],
+    }),
+  }),
+});
+
+export const { useUserLoginMutation } = authApi;
