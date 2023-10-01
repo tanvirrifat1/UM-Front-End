809efa964a80321f09531945572794dda199ef5b
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-10-01 19:06:32 +0600

    added axios instanced

---
+++ src/helpers/axios/axios.instance.ts
@@ -0,0 +1,33 @@
+import { authKey } from "@/Constants/storageKey";
+import { getToLocalStorage } from "@/utils/local-storeage";
+import axios from "axios";
+
+const instance = axios.create();
+
+instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.headers["Accept"] = "application/json";
+instance.defaults.timeout = 60000;
+
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = getToLocalStorage(authKey);
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export { instance };
