26177aa95fec09222c6ccd2cd3180c2ed9b5e364
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-10-04 09:13:22 +0600

    added table

--- src/components/ui/UMTable.tsx
+++ src/components/ui/UMTable.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Table } from "antd";
+import { Button, Table } from "antd";
 
 const UMTable = () => {
   const columns = [
@@ -12,13 +12,19 @@ const UMTable = () => {
       title: "Age",
       dataIndex: "age",
       key: "age",
+      sorter: (a: any, b: any) => a.age - b.age,
+    },
+    {
+      title: "Action",
+      render: function (data: any) {
+        return (
+          <Button onClick={() => console.log(data)} type="primary" danger>
+            x
+          </Button>
+        );
+      },
     },
   ];
-
-  const handlePageSizeChange = (page: any, pageSize: any) => {
-    console.log("page", page);
-    console.log("pageSize", pageSize);
-  };
 
   const tableData = [
     {
@@ -33,18 +39,32 @@ const UMTable = () => {
     },
   ];
 
+  const handlePageSizeChange = (page: any, pageSize: any) => {
+    console.log("page", page);
+    console.log("pageSize", pageSize);
+  };
+
+  const paginationConfig = {
+    pageSize: 5,
+    total: 10,
+    pageSizeOptions: [5, 10, 20],
+    showSizeChanger: true,
+    onChange: handlePageSizeChange,
+  };
+
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    console.log(order);
+    console.log(field);
+  };
+
   return (
     <Table
       loading={false}
       columns={columns}
       dataSource={tableData}
-      pagination={{
-        pageSize: 5,
-        total: 10,
-        pageSizeOptions: [5, 10, 20],
-        showSizeChanger: true,
-        onChange: handlePageSizeChange,
-      }}
+      pagination={paginationConfig}
+      onChange={onTableChange}
     />
   );
 };
