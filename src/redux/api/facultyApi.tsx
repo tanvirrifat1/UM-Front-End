5ccfbc78471aa08cb67e8fd76e3f344c6d2c5790
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-10-06 22:16:38 +0600

    added faculty api

---
+++ src/redux/api/facultyApi.tsx
@@ -0,0 +1,66 @@
+import { IFaculty, IMeta } from "@/types";
+import { baseApi } from "./baseApi";
+import { tagTypes } from "../slice/tag-types";
+
+const BASE_FACULTY_API = "/faculties";
+
+export const facultyApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    faculties: build.query({
+      query: (arg: Record<string, any>) => {
+        return {
+          url: BASE_FACULTY_API,
+          method: "GET",
+          params: arg,
+        };
+      },
+      transformErrorResponse: (response: IFaculty[], meta: IMeta) => {
+        return {
+          faculties: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.faculty],
+    }),
+
+    addFacultyFormData: build.mutation({
+      query: (data) => ({
+        url: "users/create-faculty",
+        method: "POST",
+        data,
+        contentType: "multipart/form-data",
+      }),
+      invalidatesTags: [tagTypes.faculty],
+    }),
+
+    faculty: build.query({
+      query: (id) => ({
+        url: `${BASE_FACULTY_API}/${id}`,
+        method: "GET",
+      }),
+      providesTags: [tagTypes.faculty],
+    }),
+    updateFaculty: build.mutation({
+      query: (data) => ({
+        url: `${BASE_FACULTY_API}/${data?.id}`,
+        method: "PATCH",
+        data: data.body,
+      }),
+      invalidatesTags: [tagTypes.faculty],
+    }),
+    deleteFaculty: build.mutation({
+      query: (id) => ({
+        url: `${BASE_FACULTY_API}/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useAddFacultyFormDataMutation, //create
+  useDeleteFacultyMutation, // delete
+  useFacultiesQuery, //get single
+  useUpdateFacultyMutation, //update
+  useFacultyQuery, // get all
+} = facultyApi;
