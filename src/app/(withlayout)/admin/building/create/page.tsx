3c907b63b603088e32dec48e7a1de91d661926f8
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-10-08 22:04:34 +0600

    added building

---
+++ src/app/(withlayout)/admin/building/create/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Form from "@/components/Forms/Form";
+import FromInput from "@/components/Forms/FromInput";
+import UMbreadCrumb from "@/components/ui/UMbreadCrumb";
+
+import { Button, Col, Row, message } from "antd";
+
+const CreateBuildPage = () => {
+  const onSubmit = async (data: any) => {
+    message.loading("Creating.....");
+    try {
+      console.log(data);
+    } catch (err: any) {
+      console.error(err.message);
+      message.error(err.message);
+    }
+  };
+  const base = "admin";
+  return (
+    <div>
+      <UMbreadCrumb
+        items={[
+          { label: `${base}`, link: `/${base}` },
+          { label: "building", link: `/${base}/building` },
+        ]}
+      />
+      <h1>Create Building</h1>
+      <Form persistKey="" submitHandler={onSubmit}>
+        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+          <Col span={8} style={{ margin: "10px 0" }}>
+            <FromInput name="title" label="Title" />
+          </Col>
+        </Row>
+        <Button type="primary" htmlType="submit">
+          add
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateBuildPage;
